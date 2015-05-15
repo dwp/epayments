a2e807c067b527c659e81875730bdf1af09bc73f
Krishan Veerappa <kv02@hotmail.co.uk>
2015-05-15 15:33:05 +0100

    Step 1 page complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
+  var errorMessage;
   $('form').parsley({
-      trigger:      'change',
+      trigger: 'change',
       successClass: "success",
       errorClass: "error",
       classHandler: function (el) {
-          return el.$element.closest('.form-group'); //working
+          return el.$element.closest('.form-group');
+      },
+      errorsContainer: function (el) {
+          return el.$element.closest('.form-group');
       },
       errorTemplate: '<span class="error-message" aria-hidden="true"></span>',
   });
 });
+
+function completeAndRedirect(){
+  location.href = "./Worldpay_HMRC_P1_OLD.php?refID="+document.getElementById('ni-number').value+"&Amount="+document.getElementById('amount-number').value;
+}
