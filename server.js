6295d8c3d2939575b312ed2b30bf3b289345fe5c
Krishan Veerappa <kv02@hotmail.co.uk>
2015-06-04 16:13:17 +0100

    ....

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require('path'),
     app = express(),
     port = (process.env.PORT || 3000),
 // Grab environment variables specified in Procfile or as Heroku config vars
-    username = process.env.USERNAME,
-    password = process.env.PASSWORD,
+    username = 'epay',
+    password = 'Summer2012',
     env = process.env.NODE_ENV || 'development';
 
 // Authenticate against the environment-provided credentials, if running
